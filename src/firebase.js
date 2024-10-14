82ae5805edfd1f030c351d59452b7410ed509947
Almaas Rizvi <almaasrizvi1101@gmail.com>
2024-10-14 21:36:21 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCPRSyIBLr866qUGnaqeUxUovMBcRKJ18I",
-  authDomain: "react-test-265c5.firebaseapp.com",
-  projectId: "react-test-265c5",
-  storageBucket: "react-test-265c5.appspot.com",
-  messagingSenderId: "481777086146",
-  appId: "1:481777086146:web:99b56f8e8f43512b37155b"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
